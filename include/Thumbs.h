b0524065f51a24ef30f4fab2de214895f8b585ab
Tony Di Croce <dicroce@gmail.com>
2015-02-20 08:22:25 -0800

    updated file headers.

--- include/Thumbs.h
+++ include/Thumbs.h
@@ -1,4 +1,14 @@
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// XSDK
+// Copyright (c) 2015 Schneider Electric
+//
+// Use, modification, and distribution is subject to the Boost Software License,
+// Version 1.0 (See accompanying file LICENSE).
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #ifndef __EXPORTY_Thumbs_h
 #define __EXPORTY_Thumbs_h
 
